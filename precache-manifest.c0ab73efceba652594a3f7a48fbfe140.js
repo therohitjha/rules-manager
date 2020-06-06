8faf4d3049cb4158e5523a4c074cb02cba6a12ec
Rohit <therohitjha@gmail.com>
2020-06-06 06:49:16 +0530

    Updates

---
+++ precache-manifest.c0ab73efceba652594a3f7a48fbfe140.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536a7bb15d335cf584b3dbd0bb75d8f0",
+    "url": "/rules-manager/index.html"
+  },
+  {
+    "revision": "faf161039233b51d9a4d",
+    "url": "/rules-manager/static/css/main.324b7294.chunk.css"
+  },
+  {
+    "revision": "ff497446983cd3a016bc",
+    "url": "/rules-manager/static/js/2.e7216dd1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/rules-manager/static/js/2.e7216dd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf161039233b51d9a4d",
+    "url": "/rules-manager/static/js/main.68439851.chunk.js"
+  },
+  {
+    "revision": "326050e835843fc2b4c6",
+    "url": "/rules-manager/static/js/runtime-main.810f1b3a.js"
+  }
+]);
